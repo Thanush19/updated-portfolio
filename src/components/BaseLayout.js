82c8e84ab7070d2e8209aecee3022c65f3772e7e
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2024-03-21 05:52:01 +0530

    achievements section added

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -6,7 +6,7 @@ import About from "./about/About";
 import Portfolio from "./portfolio/Portfolio";
 import { Route, Routes } from "react-router-dom";
 import { Box, Grid } from "@mui/material";
-
+import Achivement from "./achievements/Achivement";
 export default function BaseLayout() {
   let [darkMode, setDarkMode] = useState(false);
 
@@ -44,15 +44,9 @@ export default function BaseLayout() {
             <Route exact path={"/"} element={<Home />} />
             <Route exact path={"/about"} element={<About />} />
             <Route exact path={"/portfolio"} element={<Portfolio />} />
+            <Route exact path={"/achievement"} element={<Achivement />} />
           </Routes>
         </Grid>
-        {/* <Grid item>
-               <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
-                    py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>template created with &hearts; by <a href={'https://paytonpierce.dev'}>Payton Pierce</a></p>
-                  <p>&copy; 2023</p>
-               </Box>
-            </Grid> */}
       </Grid>
     </Box>
   );
---
+++ src/components/achievements/Achivement.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Achivement = () => {
+  return <div className="bg-black">Achivement</div>;
+};
+
+export default Achivement;
